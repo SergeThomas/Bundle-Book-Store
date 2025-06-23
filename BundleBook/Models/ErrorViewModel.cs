21f12f2d518499374b293029a02accdf8733b8b5
Serge Thomas <smughemuzi@gmail.com>
2025-06-23 11:40:08 +0200

    Add project files.

---
+++ BundleBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BundleBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
